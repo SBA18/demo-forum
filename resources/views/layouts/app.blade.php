0e08c638490d42b114b9c70653f3b6d3d578cb4c
Sawers SAW <sawers@localhost.com>
2018-04-04 14:30:04 +0200

    Bug conflict Laravel jquery and datatable.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,8 +82,9 @@
 
 
     <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    
+    {{-- <script type="text/javascript" src="{{ asset('js/app.js') }}" ></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 </html>
